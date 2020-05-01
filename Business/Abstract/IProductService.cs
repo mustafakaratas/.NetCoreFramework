6716e1d01f63e1b8bc9b39bd18edd2788c7e2c71
Mustafa Karataş <mustafakaratas190@hotmail.com>
2020-05-02 00:39:33 +0300

    add core layers

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetList();
+        List<Product> GetListByCategory(int categoryId);
+        Product Add(Product product);
+        Product Delete(Product product);
+        Product Update(Product product);
+    }
+}
---
+++ Business/Concrete/ProductService.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using Data.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public Product Add(Product product)
+        {
+            return _productRepository.Add(product);
+        }
+
+        public Product Delete(Product product)
+        {
+            return _productRepository.Delete(product);
+        }
+
+        public List<Product> GetList()
+        {
+            return _productRepository.GetAll().ToList();
+        }
+
+        public List<Product> GetListByCategory(int categoryId)
+        {
+            return _productRepository.GetAll(x => x.CategoryId == categoryId).ToList();
+        }
+
+        public Product Update(Product product)
+        {
+            return _productRepository.Update(product);
+        }
+    }
+}
